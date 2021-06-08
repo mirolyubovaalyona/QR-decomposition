1f22b8dbaf7a2bd03bb7c41234d774b508b65530
mirolyubovaalyona <niroliubova.aa@students.dvfu.ru>
2021-06-08 12:00:41 +1000

    best ver

--- QR_decomposition.py
+++ QR_decomposition.py
@@ -20,9 +20,9 @@ def gs(X, n):
 
 
 
-a=[[2, -2, 18],
-[2, 1, 0],
-[1, 2, 0]]
+a=[[1, 2, 4],
+[3, 3, 2],
+[4, 1, 3]]
 
 a=np.array(a)
 
@@ -32,7 +32,8 @@ n=len(b)
 
 
 q=gs(a, n)
-r=q.transpose().dot(a)
+r=(q.transpose().dot(a)).round(8)
+
 print(q)
 print(r)
 
